0f56558a115fe54d4ab4e6068effb6c86da0ac73
Itxaka <itxaka@kairos.io>
2025-02-14 10:02:02 +0100

    Enable error exit and fix sbctl
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- shared.sh
+++ shared.sh
@@ -13,6 +13,9 @@ NEBULA_VERSION=${NEBULA_VERSION:-}
 HELM_VERSION=${HELM_VERSION:-}
 K9S_VERSION=${K9S_VERSION:-}
 
+
+set -e
+
 if [[ "${KEEP_FILES}" == "false" && "${PUSH}" == "false" ]]; then
   printf "${RED}Both KEEP_FILES and PUSH are set to false. Please choose one or the other.\n"
   exit 0
